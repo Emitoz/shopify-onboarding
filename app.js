b96366b8dbdf49d4425775269e7dc815a93b90ad
Emmanuel Ajiboye <emmanuel.ajiboye@seerbit.com>
2023-11-27 20:18:14 +0100

    complete

--- app.js
+++ app.js
@@ -9,52 +9,91 @@
   const notificationMenu = document.querySelector("#notification-menu");
   const profileMenu = document.querySelector("#profile-menu");
 
+  const handleKeyNavigation = (e, index, allItems) => {
+    const isLast = index === allItems.length - 1;
+    const isFirst = index === 0;
+    const { key } = e;
+    const nextItem = allItems.item(index + 1);
+    const prevItem = allItems.item(index - 1);
+
+    if (key === "ArrowRight" || key === "ArrowDown") {
+      if (isLast) {
+        allItems.item(0).focus();
+        return;
+      }
+      nextItem.focus();
+    } else if (key === "ArrowLeft" || key === "ArrowUp") {
+      if (isFirst) {
+        allItems.item(allItems.length - 1).focus();
+        return;
+      }
+      prevItem.focus();
+    }
+  }
+
+  const closeNotificationMenu = () => {
+    notificationMenu.classList.remove(ITEM_OPEN);
+    notificationButton.ariaExpanded = "false";
+    notificationButton.focus();
+  }
+
+  const openNotificationMenu = () => {
+    notificationMenu.classList.add(ITEM_OPEN);
+    profileMenu.classList.remove(ITEM_OPEN);
+    notificationButton.ariaExpanded = "true";
+    const allItems = notificationMenu.querySelectorAll('[role="menuitem"]');
+    allItems.item(0).focus();
+
+    allItems.forEach((item, index) => {
+      item.addEventListener("keydown", (e) => handleKeyNavigation(e, index, allItems));
+    });
+  }
+  
   const toggleNotificationMenu = () => {
     const notificationOpen = notificationMenu.classList.contains(ITEM_OPEN);
     if (notificationOpen) {
-      notificationMenu.classList.remove(ITEM_OPEN);
+      closeNotificationMenu();
     } else {
-      notificationMenu.classList.add(ITEM_OPEN);
-      profileMenu.classList.remove(ITEM_OPEN);
+      openNotificationMenu();
+      notificationMenu.addEventListener("keyup", (e) => {
+        if (e.key === "Escape") {
+          closeNotificationMenu();
+        }
+      });
     }
+  }
+
+  const openProfileMenu = () => {
+    profileMenu.classList.add(ITEM_OPEN);
+    notificationMenu.classList.remove(ITEM_OPEN);
+    profileButton.ariaExpanded = "true";
+    const allItems = profileMenu.querySelectorAll('[role="menuitem"]');
+    allItems.item(0).focus();
+
+    allItems.forEach((item, index) => {
+      item.addEventListener("keydown", (e) => handleKeyNavigation(e, index, allItems));
+    });
+  }
+
+  const closeProfileMenu = () => {
+    profileMenu.classList.remove(ITEM_OPEN);
+    profileButton.ariaExpanded = "false";
+    profileButton.focus();
   }
 
   const toggleProfileMenu = () => {
     const profileOpen = profileMenu.classList.contains(ITEM_OPEN);
     if (profileOpen) {
-      profileMenu.classList.remove(ITEM_OPEN);
+      closeProfileMenu();
     } else {
-      profileMenu.classList.add(ITEM_OPEN);
-      notificationMenu.classList.remove(ITEM_OPEN);
+      openProfileMenu();
+      profileMenu.addEventListener("keyup", (e) => {
+        if (e.key === "Escape") {
+          closeProfileMenu();
+        }
+      });
     }
   }
-
-  const closeMenuWhenBodyIsClicked = (e) => {
-    console.log(e.target.closest(".menu") || e.target.closest(".header-buttons button") ? true : false);
-    if (e.target.closest(".profile-button") || e.target.classList.contains("profile-button")) {
-      toggleProfileMenu();
-      console.log("Profile button");
-    } else if (e.target.closest(".notificaction-button") || e.target.classList.contains("notification-button")) {
-      toggleNotificationMenu();
-      console.log("Notification button");
-    } else if (e.target.closest(".menu")) {
-      return;
-    } else {
-      notificationMenu.classList.remove(ITEM_OPEN);
-      profileMenu.classList.remove(ITEM_OPEN);
-    }
-
-    return;
-
-      // if (
-      //   !e.target.closest(".menu") || !e.target.closest(".header-buttons button")
-      // ) {
-      //   notificationMenu.classList.remove(ITEM_OPEN);
-      //   profileMenu.classList.remove(ITEM_OPEN);
-      // }
-  }
-
-  // document.body.addEventListener("click", (e) => closeMenuWhenBodyIsClicked(e));
 
   notificationButton.addEventListener("click", toggleNotificationMenu);
   profileButton.addEventListener("click", toggleProfileMenu);
@@ -65,10 +104,9 @@
 
   closeAlertButton.addEventListener("click", () => alertContainer.classList.add(ITEM_HIDDEN));
 
-
   // Expand and collapse setup
   const expandButton = document.querySelector("#expand-button");
-  const collapseButton = document.querySelector("#collapse-button");
+  // const collapseButton = document.querySelector("#collapse-button");
   const collapseSection = document.querySelector("#setup-collapse");
 
   const toggleCollapse = () => {
@@ -76,60 +114,111 @@
 
     if (sectionHidden) {
       collapseSection.classList.remove(ITEM_HIDDEN);
-      expandButton.classList.add(ITEM_HIDDEN);
-      collapseButton.classList.remove(ITEM_HIDDEN);
+      // expandButton.classList.add(ITEM_HIDDEN);
+      // collapseButton.classList.remove(ITEM_HIDDEN);
+      expandButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <path fill-rule="evenodd" clip-rule="evenodd"
+            d="M14.5303 12.2803C14.2374 12.5732 13.7626 12.5732 13.4697 12.2803L10 8.81066L6.53033 12.2803C6.23744 12.5732 5.76256 12.5732 5.46967 12.2803C5.17678 11.9874 5.17678 11.5126 5.46967 11.2197L9.46967 7.21967C9.76256 6.92678 10.2374 6.92678 10.5303 7.21967L14.5303 11.2197C14.8232 11.5126 14.8232 11.9874 14.5303 12.2803Z"
+            fill="#4A4A4A" />
+        </svg>
+      `;
     }  else {
       collapseSection.classList.add(ITEM_HIDDEN);
-      collapseButton.classList.add(ITEM_HIDDEN);
-      expandButton.classList.remove(ITEM_HIDDEN);
+      expandButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+          <path fill-rule="evenodd" clip-rule="evenodd"
+            d="M5.71967 8.46967C6.01256 8.17678 6.48744 8.17678 6.78033 8.46967L10.25 11.9393L13.7197 8.46967C14.0126 8.17678 14.4874 8.17678 14.7803 8.46967C15.0732 8.76256 15.0732 9.23744 14.7803 9.53033L10.7803 13.5303C10.4874 13.8232 10.0126 13.8232 9.71967 13.5303L5.71967 9.53033C5.42678 9.23744 5.42678 8.76256 5.71967 8.46967Z"
+            fill="#4A4A4A" />
+        </svg>
+      `;
     }
   }
 
   expandButton.addEventListener("click", toggleCollapse);
-  collapseButton.addEventListener("click", toggleCollapse);
 
+  // Open and close items
+  const setupItems = document.querySelectorAll(".setup-item");
 
-  const checkboxItem = document.querySelector(".setup-checkbox");
-  const incompleteIcon = document.querySelector("svg.incomplete-icon");
-  const completedIcon = document.querySelector("svg.completed-icon");
-  const loadingSpinner = document.querySelector("svg.loading-spinner");
-  const buttonStatus = document.querySelector(".item-status");
-
-  function markAsDone () {
-    incompleteIcon.classList.add(ITEM_HIDDEN);
-    loadingSpinner.classList.remove(ITEM_HIDDEN);
-    buttonStatus.ariaLabel = "Loading. Please wait...";
-    setTimeout(() => {
-      loadingSpinner.classList.add(ITEM_HIDDEN);
-      completedIcon.classList.remove(ITEM_HIDDEN);
-    }, 2000);
-    buttonStatus.ariaLabel = "Successfully marked task as done.";
-    checkboxItem.ariaLabel = checkboxItem.ariaLabel.replace("as done", "as not done");
-    checkboxItem.classList.add(ITEM_DONE);
+  function getAllSiblings(element, parent) {
+    const children = [...parent.children];
+    return children.filter(child => child !== element);
   }
 
-  function markAsNotDone () {
-    completedIcon.classList.add(ITEM_HIDDEN);
-    loadingSpinner.classList.remove(ITEM_HIDDEN);
-    buttonStatus.ariaLabel = "Loading. Please wait...";
-    setTimeout(() => {
-      loadingSpinner.classList.add(ITEM_HIDDEN);
-      incompleteIcon.classList.remove(ITEM_HIDDEN);
-    }, 2000);
-    buttonStatus.ariaLabel = "Successfully marked task as not done.";
-    checkboxItem.ariaLabel = checkboxItem.ariaLabel.replace("as not done", "as done");
-    checkboxItem.classList.remove(ITEM_DONE);
-  }
+  setupItems.forEach((ele) => {
+    ele.addEventListener("click", function() {
+      if (!this.classList.contains(ITEM_OPEN)) {
+        this.classList.add(ITEM_OPEN);
+        getAllSiblings(ele, collapseSection).forEach(el => {
+          el.classList.remove(ITEM_OPEN);
+        })
+        return;
+      }
+      return;
+    });
+  });
+
+  const progressText = document.querySelector("#progress-text");
+  const progressBar = document.querySelector("#progress");
+  let progress = 0;
   
-  function toggleStatus () {
-    const itemIsDone = checkboxItem.classList.contains(ITEM_DONE);
-
-    if (itemIsDone) {
-      markAsNotDone();
-    } else {
-      markAsDone();
+  setupItems.forEach((ele) => {
+    const checkboxItem = ele.querySelector(".setup-checkbox");
+    const incompleteIcon = ele.querySelector("svg.incomplete-icon");
+    const completedIcon = ele.querySelector("svg.completed-icon");
+    const loadingSpinner = ele.querySelector("svg.loading-spinner");
+    const buttonStatus = ele.querySelector(".item-status");
+  
+    const markAsDone = () => {
+      incompleteIcon.classList.add(ITEM_HIDDEN);
+      loadingSpinner.classList.remove(ITEM_HIDDEN);
+      buttonStatus.ariaLabel = "Loading. Please wait...";
+      setTimeout(() => {
+        loadingSpinner.classList.add(ITEM_HIDDEN);
+        completedIcon.classList.remove(ITEM_HIDDEN);
+        progress++;
+        progressText.innerHTML = `${progress.toString()}/5`;
+        progressBar.style.width = `${(progress / 5) * 100}%`;
+        if (progress === 5) {
+          progressBar.style.borderRadius = "28px";
+        }
+      }, 1000);
+      buttonStatus.ariaLabel = "Successfully marked task as done.";
+      checkboxItem.ariaLabel = checkboxItem.ariaLabel.replace("as done", "as not done");
+      checkboxItem.classList.add(ITEM_DONE);
     }
-  }
+  
+    const markAsNotDone = () => {
+      completedIcon.classList.add(ITEM_HIDDEN);
+      loadingSpinner.classList.remove(ITEM_HIDDEN);
+      buttonStatus.ariaLabel = "Loading. Please wait...";
+      setTimeout(() => {
+        loadingSpinner.classList.add(ITEM_HIDDEN);
+        incompleteIcon.classList.remove(ITEM_HIDDEN);
+        progress--;
+        progressText.innerHTML = `${progress.toString()}/5`;
+        progressBar.style.width = `${(progress / 5) * 100}%`;
+        progressBar.style.borderTopRightRadius = "0";
+        progressBar.style.borderBottomRightRadius = "0";
+      }, 1000);
+      buttonStatus.ariaLabel = "Successfully marked task as not done.";
+      checkboxItem.ariaLabel = checkboxItem.ariaLabel.replace("as not done", "as done");
+      checkboxItem.classList.remove(ITEM_DONE);
+    }
+    
+    const toggleStatus = () => {
+      const itemIsDone = checkboxItem.classList.contains(ITEM_DONE);
+  
+      if (itemIsDone) {
+        markAsNotDone();
+      } else {
+        markAsDone();
+      }
+    }
+    
+    checkboxItem.addEventListener("click", () => {
+      toggleStatus()
+    });
+  });
 
-  checkboxItem.addEventListener("click", toggleStatus);
 })()
